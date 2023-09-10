d2939f589957518fe1d4d92b6dae57e64fed8493
LogicEu <eugenioarteagaa@gmail.com>
2023-09-10 20:36:58 -0300

    Added shared and static functions to Makefile and build.sh to easily compile spxe.h as static or dynamic library. Included static and shared libraries in installation, placing compiled libraries in /usr/local by default. Added these new binaries to .gitignore

--- Makefile
+++ Makefile
@@ -1,34 +1,61 @@
 # spxe Makefile
 
 EXE=a.out
-HEADER=spxe.h
-EXAMPLES=examples
-SRC=$(EXAMPLES)/hello.c
-SCRIPT=build.sh
+NAME=spxe
+HEADER=$(NAME).h
+OBJ=$(NAME).o
+LIB=lib$(NAME)
 
 CC=gcc
 STD=-std=c89
 OPT=-O2
 WFLAGS=-Wall -Wextra -pedantic
-INC=-I.
-LIB=-lglfw
+LIBS=-lglfw
+
+EXAMPLES=examples
+SRC=$(EXAMPLES)/hello.c
+SCRIPT=build.sh
 
 OS=$(shell uname -s)
 ifeq ($(OS),Darwin)
-    OPNGL=-framework OpenGL
+    LIBS+=-framework OpenGL
+    DLIB=-dynamiclib
+    SUFFIX=dylib
 else
-    OPNGL=-lm -lGL -lGLEW
+    LIBS+=-lm -lGL -lGLEW
+    DLIB=-fPIC -shared
+    SUFFIX=so
 endif
 
-CFLAGS=$(STD) $(OPT) $(WFLAGS) $(INC) $(LIB) $(OPNGL)
+CFLAGS=$(STD) $(OPT) $(WFLAGS)
 
 $(EXE): $(SRC)
 
 $(EXAMPLES)/%.c: $(HEADER)
-	$(CC) $@ -o $(EXE) $(CFLAGS)
+	$(CC) $(CFLAGS) -I. $(LIBS) $@ -o $(EXE) 
+
+.PHONY: shared static clean install uninstall
+
+all: shared static
+
+shared: $(LIB).$(SUFFIX)
+
+static: $(LIB).a
+
+$(LIB).$(SUFFIX): $(OBJ)
+	$(CC) $(OPT) $(LIBS) $(DLIB) $^ -o $@  
+
+$(LIB).a: $(OBJ)
+	ar -cr $@ $^
+
+$(OBJ): $(HEADER)
+	$(CC) $(CFLAGS) -x c -DSPXE_APPLICATION -c $^ -o $@ 
 
 clean:
 	$(RM) $(EXE)
+	$(RM) $(OBJ)
+	$(RM) $(LIB).$(SUFFIX)
+	$(RM) $(LIB).a
 
 install: $(SCRIPT)
 	./$< $@
--- build.sh
+++ build.sh
@@ -1,89 +1,117 @@
 #!/bin/bash
 
-exe=a.out
-header=spxe.h
-installpath=/usr/local/include
-
 cc=gcc
+exe=a.out
+name=spxe
+
+lib=lib$name
+header=$name.h
+obj=$name.o
+libs=(-lglfw)
 std=-std=c89
 opt=-O2
-inc=-I.
-lib=-lglfw
 
-wflag=(
+path=/usr/local
+incpath=$path/include
+libpath=$path/lib
+
+cflags=(
+    $std
+    $opt
     -Wall
     -Wextra
     -pedantic
 )
 
-linux=(
-    -lm     # used by examples/pendulum and examples/mandelbrot.c
-    -lGL
-    -lGLEW
-)
-
-mac=(
-    -framework OpenGL
-)
-
+if echo "$OSTYPE" | grep -q "darwin"; then
+    libs+=(-framework OpenGL)
+    dlib=(-dynamiclib)
+    suffix=dylib
+elif echo "$OSTYPE" | grep -q "linux"; then
+    libs+=(-lm -lGL -lGLEW)
+    dlib=(-fPIC -shared)
+    suffix=so
+fi
+    
 cmd() {
     echo "$@" && $@
 }
 
-compile() {
-    if echo "$OSTYPE" | grep -q "darwin"; then
-        os=${mac[*]}
-    elif echo "$OSTYPE" | grep -q "linux"; then
-        os=${linux[*]}
-    else
-        echo "this OS is not supported by this build script yet..."
-    fi
-    
-    cmd $cc $1 -o $exe $std $opt ${wflag[*]} $inc $lib ${os[*]}
+compile() {    
+    cmd $cc ${cflags[*]} -I. ${libs[*]} $1 -o $exe 
+}
+
+object() {
+    cmd $cc ${cflags[*]} -x c -DSPXE_APPLICATION -c $header -o $obj
+}
+
+shared() {
+    cmd $cc $opt ${libs[*]} ${dlib[*]} $obj -o $lib.$suffix
+}
+
+static() {
+    cmd ar -cr $lib.a $obj
 }
 
 cleanf() {
     [ -f $1 ] && cmd rm -f $1
 }
 
+clean() {
+    cleanf $exe
+    cleanf $obj
+    cleanf $lib.a
+    cleanf $lib.$suffix
+}
+
 install() {
     [ "$EUID" -ne 0 ] && echo "run with 'sudo' to install" && exit
-    cmd cp $header $installpath
-    echo "successfully installed $header"
+    [ -f $obj ] || object
+    [ -f $lib.$suffix ] || shared
+    [ -f $lib.a ] || static
+    cmd cp $header $incpath
+    cmd cp $lib.$suffix $libpath
+    cmd cp $lib.a $libpath
     return 0
 }
 
 uninstall() {
     [ "$EUID" -ne 0 ] && echo "run with 'sudo' to uninstall" && exit
-    cleanf $installpath/$header
-    echo "successfully uninstalled $header"
+    cleanf $incpath/$header
+    cleanf $libpath/$lib.$suffix
+    cleanf $libpath/$lib.a
     return 0
-}
-
-fail() {
-    echo "$0: file '$1' not found"
 }
 
 usage() {
     echo "$0 usage:"
-    echo -e "<source>\t: Compile <source> file with $header"
-    echo -e "help\t\t: Print usage information and available commands"
-    echo -e "clean\t\t: Delete compiled executable"
-    echo -e "install\t\t: Install $header in $installpath (run with sudo)"
-    echo -e "uninstall\t: Unnstall $header from $installpath (run with sudo)"
+    echo -e "<source>\t: compile <source> file including $header"
+    echo -e "shared\t\t: build $name as a shared library: $lib.$suffix"
+    echo -e "static\t\t: build $name as a static library: $lib.a"
+    echo -e "all\t\t: build $name as shared and static libraries"
+    echo -e "help\t\t: print usage information and available commands"
+    echo -e "clean\t\t: delete local builds and executables"
+    echo -e "install\t\t: install $name in $path (run with sudo)"
+    echo -e "uninstall\t: uninstall $name from $path (run with sudo)"
 }
 
 (( $# < 1 )) && usage && exit
 
 case "$1" in
+    "shared")
+        object && shared;;
+    "static")
+        object && static;;
+    "all")
+        object && shared && static;;
     "help")
         usage;;
     "clean")
-        cleanf $exe;;
+        clean;;
     "install")
         install;;
     "uninstall")
         uninstall;;
     *)
-        [ -f $1 ] && compile $1 || fail $1;;
+        [ -f $1 ] && compile $1 || echo "$0: file '$1' not found";;
 esac
